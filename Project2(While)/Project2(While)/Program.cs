9814a57a4649d82d0dd9ff1c6a66112fcaa1f7fb
Elysium-0 <e77229983@gmail.com>
2025-08-14 10:26:12 +0300

    Project2(While)

---
+++ Project2(While)/Project2(While)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int quantity;
+            char bleba;
+            Console.WriteLine("Какое количество строк вы желаете: ");
+            if (!int.TryParse(Console.ReadLine(), out quantity) || quantity <= 0)
+            {
+                Console.WriteLine("Пожалуйста введите целоцисленое положительное число:");
+                continue;
+            }
+            string[] inputs = new string[quantity];
+            for (int i1 = 0; i1 < quantity; i1++)
+            {
+                Console.WriteLine("Введите строку №" + (i1 + 1));
+                inputs[i1] = Console.ReadLine();
+            }
+            string combinedinputs = string.Join("", inputs);
+            int totalCharacters = combinedinputs.Length; //общее количество символов 
+            Console.WriteLine("Введите символ что хотите подсчитать: ");
+            bleba = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            int count = 0;
+            int i = 0;
+            while (i < combinedinputs.Length)
+            {
+                i++;
+                if (combinedinputs[i] == bleba)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Количество символов '{bleba}': {count}");
+            Console.WriteLine("Хотите продолжить(да или нет): ");
+            string con = Console.ReadLine();
+            if (con.ToLower() != "да")
+            {
+                break;
+            }
+        }
+    }
+}
+//Пользователя спрашивают, сколько строк он хочет ввести
+//Пользователь вводит эти строки поочерёдно
+//Программа объединяет их в одну строку, считает число символов
+//Программа спрашивает, число каких символов в предложении посчитать (необходимо выполнить с помощью всех трех типов циклов в C#)
+//Программа выводит его
+
+// Сделано на while
